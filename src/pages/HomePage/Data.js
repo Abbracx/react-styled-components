6bc501daf8f9acfd3c11889a9a9683f1a6be1870
Abbracx <tankoraphael@gmail.com>
2023-05-05 15:16:22 +0100

    Added styled components for HomePage

--- src/pages/HomePage/Data.js
+++ src/pages/HomePage/Data.js
@@ -1,3 +1,15 @@
 export const homeObjOne = {
-    lightBg: false
+    lightBg: false,
+    imgStart: '',
+    lightTopLine: true, 
+    lightText: true,
+    lightTextDesc: true,
+    buttonLabel: 'Get Started',
+    description: 'We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals.',
+    headline: 'Lead generational specialist for online businesses',
+    topLine: 'Marketing Agency',
+    primary: true,
+    img: '',
+    alt: 'Image',
+    start: ''
 }
